6e93e1c52a9f865ca2e208e8d2f7f5329945597a
Natalia <natemaciel7@gmail.com>
2025-02-15 16:13:52 -0300

    post-add

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+let shoppingList = [];
+let currentId = 1;
+
 const express = require("express");
 
 const app = express();
@@ -7,4 +10,17 @@ const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+app.post("/items", (req, res) => {
+  const { name, quantity, type } = req.body;
+  if (!name || !quantity || type)
+    return res.status(422).json({ error: "todos os campos são obrigatórios" });
+  const itemExists = shoppingList.some((item) => item.name === name);
+  if (itemExists) {
+    return res.status(409).json({ error: "item já existe na lista" });
+  }
+  const newItem = { id: currentId++, name, quantity, type };
+  shoppingList.push(newItem);
+  res.status(201).json(newItem);
 });
